dedaf6d7e936d0da1305bf8298ab8ba9939f449d
MAY 06 <MAY 06@LAB2-34>
2024-11-25 08:11:02 +0700

    Add project files.

---
+++ bai18_tinhtisothoc/bai18_tinhtisothoc.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "tinh so thoc" << endl;
+	int o, tongsothoc = 0;
+	cout << "nhap o:"; cin >> o;
+	for (int i = 0; i <=0; i++)
+	{
+		tongsothoc = 7;
+	}
+	cout << 0 << "i="<< tongsothoc;
+		return 0;
+}
